c8beeab8c3b5a4083f23542de4c98da70fabc683
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2023-12-06 22:50:59 +0800

    Making a hover-effects
    * For now the hover effect is not working
        - there are needs to be resolve and find what cause of hovers not working properly

--- src/components/theme/Form.jsx
+++ src/components/theme/Form.jsx
@@ -8,7 +8,7 @@ export default function Form() {
           placeholder="email@company.com"
           className="form__input"
         />
-        <button className="form__button">
+        <button id="randomKey" className="form__button">
           Subscribe to monthly newsletter
         </button>
       </form>
