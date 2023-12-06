2558cef73f55c294803e10d2969688adb45ab5a4
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2023-12-06 14:40:21 +0800

    Setting Up style / component files 📂📝
    * `css` folder has been added for the css files that created by Live SASS Compiler
    
    * `scss` folder has been added for the scss files that will be used to style the pages
    
    * `variable` folder has been added for the variable that will be used across scss files.
    
    * `components` folder has been added for the jsx files that will be used as a component (to make the component easy to find and managable)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,18 @@
 export default function Footer() {
   return (
-    <div className="attribution">
-      Challenge by{' '}
-      <a
-        href="https://www.frontendmentor.io?ref=challenge"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Frontend Mentor
-      </a>
-      . Coded by <a href="#">Your Name Here</a>.
-    </div>
+    <>
+      <div className="attribution">
+        <small>
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>{' '}
+          Challenge by . Coded by <a href="#">Your Name Here</a>.
+        </small>
+      </div>
+    </>
   );
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,17 +1,19 @@
 export default function SignUp() {
   return (
-    <div>
-      <h1>Stay updated!</h1>
-      <h2>Join 60,000+ product managers receiving monthly updates on:</h2>
-      <p>
-        Product discovery and building what matters Measuring to ensure updates
-        are a success And much more!
-      </p>
+    <>
       <div>
-        <h3>Email address</h3>
-        <h4>email@company.com</h4>
-        <p>Subscribe to monthly newsletter</p>
+        <h1>Stay updated!</h1>
+        <h2>Join 60,000+ product managers receiving monthly updates on:</h2>
+        <p>
+          Product discovery and building what matters Measuring to ensure
+          updates are a success And much more!
+        </p>
+        <div>
+          <h3>Email address</h3>
+          <h4>email@company.com</h4>
+          <p>Subscribe to monthly newsletter</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
