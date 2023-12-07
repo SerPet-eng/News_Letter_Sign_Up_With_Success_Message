57cb14618b7332cd73ffff08a766e0f0588c6616
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2023-12-07 16:50:58 +0800

    Update Form.jsx 🛠️📃
    * useState were added for the value that will be inputted in an input form. Also error state as well
    
        - this will be useful for conditional statement

--- src/components/theme/Form.jsx
+++ src/components/theme/Form.jsx
@@ -1,19 +1,33 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Form() {
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  const handleSignUp = () => {
-    navigate('/success');
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    if (value) {
+      navigate('/success');
+      setError(null);
+    } else {
+      setError('Empty/Invalid Email!');
+    }
   };
 
   return (
     <>
       <form className="form">
-        <p className="form__title">Email address</p>
+        <div className="form__placeholder">
+          <p className="form__title">Email address</p>
+          {error && <small className="form__error">{error}</small>}
+        </div>
         <input
-          type="text"
+          type="email"
+          value={value}
           placeholder="email@company.com"
+          onChange={(e) => setValue(e.target.value)}
           className="form__input"
         />
         <button id="randomKey" className="form__button" onClick={handleSignUp}>
