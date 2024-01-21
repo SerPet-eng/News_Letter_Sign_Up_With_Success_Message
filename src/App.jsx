a027a18b101ff6527ccdd09a310bab8a7cf913b5
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2024-01-21 09:27:27 +0800

    Adding NotFound.jsx🛠️👷‍♂️
     - To catch the error if the page has not found.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import Success from './components/Success';
 import Footer from './components/Footer';
+import NotFound from './components/NotFound';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<SignUp />} />
           <Route path="/success" element={<Success />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,16 @@
+export default function NotFound() {
+  return (
+    <>
+      <section className="error">
+        <h1>Not Found 404.</h1>
+        <h2>Sorry the page you are accessing is not available.</h2>
+        <small>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia
+          recusandae dignissimos culpa nesciunt modi quaerat! At ab ratione
+          aperiam, iusto similique blanditiis reiciendis, aut sequi id
+          necessitatibus, ducimus nisi qui.
+        </small>
+      </section>
+    </>
+  );
+}
